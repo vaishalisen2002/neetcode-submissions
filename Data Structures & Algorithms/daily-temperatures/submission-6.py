9b44060345ddc4674854891dfdfe02625ead5b66
Vaishali Sen <114022721+vaishalisen2002@users.noreply.github.com>
2026-05-29 12:20:50 +0530

    Add: daily-temperatures - submission-6

---
+++ Data Structures & Algorithms/daily-temperatures/submission-6.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        res=[0] * len(temperatures)
+        stack= [] #(temp,index)
+
+        for i,t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT,stackInd = stack.pop()
+                res[stackInd] = (i - stackInd)
+            stack.append([t,i])
+        return res        
+        
